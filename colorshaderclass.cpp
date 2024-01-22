cabf9cfc11315fe425b9f53716b3e4568ab0a8bd
Owen West <west.owen001@gmail.com>
2024-01-22 12:55:12 -0500

    shader class addition

---
+++ colorshaderclass.cpp
@@ -0,0 +1,16 @@
+#include "colorshaderclass.h"
+
+ColorShaderClass::ColorShaderClass() {
+	m_vertexShader = 0;
+	m_pixelShader = 0;
+	m_layout = 0;
+	m_matrixBuffer = 0;
+}
+
+ColorShaderClass::ColorShaderClass(const ColorShaderClass& other) {
+
+}
+
+ColorShaderClass::~ColorShaderClass() {
+
+}
